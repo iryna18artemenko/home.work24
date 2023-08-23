ef308fce3d69da1eee3c69c6595240a823f9f337
Iryna Artemenko <136636205+iryna18artemenko@users.noreply.github.com>
2023-08-23 13:06:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+text.onmouseover = function () {
+    document.getElementById("text").innerText ="<div>Наведи на текст</div>";
+}
+
+text.onmouseout = function () {
+    document.getElementById("text").innerText ="Наведи на текст";
+}
+
